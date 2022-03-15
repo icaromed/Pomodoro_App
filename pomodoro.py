172de801c4dee77a363dbb1497fb8c686251effa
gamer hard99 <icaromeddev@gmail.com>
2022-03-15 20:13:01 -0300

    Function added: find_sleep  | Using .format instead of f-strings | Eemoved inputs that aren't sleep and duration of sleep

--- pomodoro.py
+++ pomodoro.py
@@ -2,15 +2,31 @@ class Pomo(object):
 
     def __init__ (self, wakeup, sleep, lunch):
 
+        self.day = list(range(1,25))
         self.wakeup = wakeup
         self.sleep = sleep
         self.lunch = lunch
-    def __str__ (self):
-
-        return f"Wake up at {self.wakeup}h\nLunch at {self.lunch}h\nSleep at {self.sleep}h"
     
-wakeup  =input("When do you wanna wake up? (01-24)")
-sleep = input("When do you wanna sleep (01-24)?")
-lunch = input("When do you wanna lunch (01-24)?")
+    def __str__ (self):
+        return "Wake up at {}h\nLunch at {}h\nSleep at {}h".format(self.wakeup,self.lunch, self.sleep)
+    
+    
+    def find_sleep(self):
+        for position in range(1,int(self.wakeup)):
+            self.remove = int(self.sleep)+position
+            if self.remove >24:
+                self.remove -= 24 
+            self.day.remove(self.remove)
+        return "Your sleep starts at {}h and finishs at {}h!".format(self.day[-1],self.day[0])
 
-print(Pomo(wakeup,sleep,lunch))                                                                                                                                  
+            
+        
+
+
+
+
+sleep = input("When do you wanna sleep (01-24)?")
+wakeup = input("How much hours of sleep do you want? (01-24)")
+
+x = Pomo(wakeup,sleep,lunch)
+print(x.find_sleep())                                                                                                                                 
