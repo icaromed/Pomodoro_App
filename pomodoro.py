51e3fdcbea34342aabd61fe368a54fa8003b08e0
gamer hard99 <icaromeddev@gmail.com>
2022-03-16 09:35:14 -0300

    almost everything changed | Pomo can return two kinds of string | another file was created for getting input and calling functions

--- pomodoro.py
+++ pomodoro.py
@@ -1,32 +1,31 @@
 class Pomo(object):
+    def __init__ (self, sleep, sleep_hours):
 
-    def __init__ (self, wakeup, sleep, lunch):
-
-        self.day = list(range(1,25))
-        self.wakeup = wakeup
+        self.sleep_hours = sleep_hours
         self.sleep = sleep
-        self.lunch = lunch
-    
+
     def __str__ (self):
-        return "Wake up at {}h\nLunch at {}h\nSleep at {}h".format(self.wakeup,self.lunch, self.sleep)
-    
-    
-    def find_sleep(self):
-        for position in range(1,int(self.wakeup)):
+        return "Sleep at {}h for {} hours.".format(self.sleep, self.sleep_hours)
+
+
+    def remove_sleep_time(self):
+        self.full_day = list(range(1,25))
+        for position in range(1,int(self.sleep_hours)):
             self.remove = int(self.sleep)+position
             if self.remove >24:
                 self.remove -= 24 
-            self.day.remove(self.remove)
-        return "Your sleep starts at {}h and finishs at {}h!".format(self.day[-1],self.day[0])
+            self.full_day.remove(self.remove)
 
-            
-        
+        return "Sleep: {}h\r\nWakeup: {}h".format(self.full_day[-1],self.full_day[0])
 
 
+#testing with 22 hours and 6 hours
+if __name__=="__main__":
+    print(Pomo(22,6).remove_sleep_time())      
+    
 
 
-sleep = input("When do you wanna sleep (01-24)?")
-wakeup = input("How much hours of sleep do you want? (01-24)")
 
-x = Pomo(wakeup,sleep,lunch)
-print(x.find_sleep())                                                                                                                                 
+
+
+                                                                                                                              
