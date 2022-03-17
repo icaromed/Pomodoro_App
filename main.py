82a0c1d77cc8f0dfe339014c7be603efb286d903
gamer hard99 <icaromeddev@gmail.com>
2022-03-17 19:18:57 -0300

    sleeping schedule almost ready | there are still some bugs to fix

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import pomodoro
 
 def sleep():
-    sleep_time = int(input("When do you wanna sleep? (01-24) "))
-    sleep_hours = int(input("How much hours of sleep do you want? (01-24) "))
+    accept = input("Welcome to the Pomodoro Routine App\r\nWould you like to create your custom routine schedule? (yes|no) ").lower().strip()
+    
+    if accept == "yes":
+        sleep_messages()
+    
+    print("Thanks for using our app.")
 
-    #print(pomodoro.Pomo(sleep_time, sleep_hours))
-    print(pomodoro.Pomo(sleep_time, sleep_hours).remove_sleep_time())
+def sleep_messages():
+    sleep_time = int(input("When do you wanna sleep? (01-24) "))
+    sleep_hours = int(input("How many hours of sleep is good for you? (01-24) "))
+    
+    if sleep_hours > 24:
+        if input("WoW! That's a lot of sleep.\r\nAre you sure about that? (yes|no) ").lower().strip()!="yes":
+            sleep_messages()
+    
+    pomodoro.Pomo(sleep_time, sleep_hours).sleeping()
 
 
 if __name__ == "__main__":
     sleep()
+
+#implementations:
+
+#choice of AM/PM or 24h
+#fix error of str at input
+#add sleep > 24 -- number of days
--- pomodoro.py
+++ pomodoro.py
@@ -13,24 +13,17 @@ class Pomo(object):
         while self.wake>24:
             self.wake -= 24
 
-    def __str__ (self):
-        return "Sleep at {:} h for {:} h.".format(self.sleep, self.sleep_hours)
+    def sleeping (self):
+        print ("Great! So you're sleeping at {:}h and waking up at {:}h. Those are {:} hours of sleep."
+        .format(self.sleep, self.wake,self.sleep_hours))
+        if(input("Is that fine for you? (yes|no) ").lower().strip()!="yes"):
+            sleep_hours = input("Type a new number of hours: ")
 
-
-    def remove_sleep_time(self):
-        self.full_day = list(range(1,25))
-        
-        for position in range(1,self.sleep_hours):
-            self.remove = self.sleep + position
-            while self.remove > 24:
-                self.remove -= 24 
-            self.full_day.remove(self.remove)
-        return "Sleep: {}h\r\nWakeup: {}h".format(self.sleep, self.wake)
 
 
 #for testing
 if __name__=="__main__":
-    print(Pomo(1,6).remove_sleep_time())      
+    pass  
     
 
 
