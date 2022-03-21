7629ce237f85f2a6b8ed705a1dbfce6d44a80588
gamer hard99 <icaromeddev@gmail.com>
2022-03-21 09:02:32 -0300

    Put all the functions inside pomodoro.py | function sleep_message was removed

--- main.py
+++ main.py
@@ -1,29 +1,12 @@
 import pomodoro
 
-def sleep():
-    accept = input("Welcome to the Pomodoro Routine App\r\nWould you like to create your custom routine schedule? (yes|no) ").lower().strip()
+def run():
+       
+    if input("Welcome to the Pomodoro Routine App\r\n\r\nWould you like to create your custom routine schedule? (yes|no) ").lower().strip() == "yes":
+        pomodoro.Pomo().sleeping()
     
-    if accept == "yes":
-        sleep_messages()
-    
-    print("Thanks for using our app.")
-
-def sleep_messages():
-    sleep_time = int(input("When do you wanna sleep? (01-24) "))
-    sleep_hours = int(input("How many hours of sleep is good for you? (01-24) "))
-    
-    if sleep_hours > 24:
-        if input("WoW! That's a lot of sleep.\r\nAre you sure about that? (yes|no) ").lower().strip()!="yes":
-            sleep_messages()
-    
-    pomodoro.Pomo(sleep_time, sleep_hours).sleeping()
+    print("\r\nThanks for using our app.")
 
 
 if __name__ == "__main__":
-    sleep()
-
-#implementations:
-
-#choice of AM/PM or 24h
-#fix error of str at input
-#add sleep > 24 -- number of days
+    run()
--- pomodoro.py
+++ pomodoro.py
@@ -1,33 +1,44 @@
 class Pomo(object):
-    def __init__ (self, sleep, sleep_hours):
-        
-        self.sleep = sleep
-        while self.sleep > 24:
-            self.sleep -= 24
+    
+    def __init__ (self):
+        self.__name = input("What is your name? ")
 
-        self.sleep_hours = sleep_hours
-        while self.sleep_hours > 24:
-            self.sleep_hours -= 24
-        
-        self.wake = self.sleep + self.sleep_hours
-        while self.wake>24:
-            self.wake -= 24
+        self.__sleep = (int(input("When do you wanna sleep? (01-24) ")))
+
+        self.__sleep_hours = int(input("How many hours of sleep is good for you? (01-24) "))
+    
+    global time_ad
+    @staticmethod 
+    def time_ad(time):
+        while time > 24:
+            time -=24
+        return time
 
     def sleeping (self):
-        print ("Great! So you're sleeping at {:}h and waking up at {:}h. Those are {:} hours of sleep."
-        .format(self.sleep, self.wake,self.sleep_hours))
-        if(input("Is that fine for you? (yes|no) ").lower().strip()!="yes"):
-            sleep_hours = input("Type a new number of hours: ")
+        self.checking = False
+        while self.checking == False:
+            print ("Great! So you're sleeping at {:}h and waking up at {:}h. Those are {:} hours of sleep."
+            .format(time_ad(self.__sleep), time_ad(self.__sleep + self.__sleep_hours) ,time_ad(self.__sleep_hours)))
+            
+            if(input("Is that fine for you? (yes|no) ").lower().strip()!="yes"):
+                self.sleep_hours = int(input("Type a new number of sleeping hours: "))
+            else:
+                self.checking = True
+    
 
+    @property
+    def sleep_hours(self):
+        return self._Pomo__sleep_hours
 
+    @sleep_hours.setter
+    def sleep_hours(self, new_hours):
+        self.__sleep_hours = new_hours
 
 #for testing
 if __name__=="__main__":
-    pass  
-    
+    test = Pomo().sleeping()
 
+#implementations:
 
-
-
-
-                                                                                                                              
+#choice of AM/PM or 24h
+#fix error of str at input                                                                                                                            
