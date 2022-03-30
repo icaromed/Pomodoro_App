ed15ad63ef6893db3afe20810940bbc765e301cd
gamer hard99 <icaromeddev@gmail.com>
2022-03-30 07:38:31 -0300

    New: starting the implementation of pomodoro timer

--- user.py
+++ user.py
@@ -1,25 +1,61 @@
-from pomodoro import Pomo
+from sleep import Sleep
 from datetime import datetime
+from pomodoro import Pomodoro
 
-class User(Pomo):
+
+class User(Sleep, Pomodoro):
     
     def __init__(self):
         
         self._name = input("What is your name? ")
-        
+        self._avaiable_range = self.available_time()
+        self.pomodoro_timer()
+
+    def available_time(self):   
         if input("Would you like to create your custom routine schedule? (yes|no) ").lower().strip() == "yes":
             
             super().__init__(input("\r\nChoose you preference: ('12' for 12 hours format or '24' for 24 hours format) "))   
             if self.error == False:
                 self.sleeping()
-                time_range = self.activity_range()
-                actual_time = datetime.now().strftime('%H')
+                return self.activity_range()
                 
-                if int(actual_time) in time_range:
-                    print("Okay, time to Pomo!")
+    def pomodoro_timer(self):
+        self.actual_time = int(datetime.now().strftime('%H'))
+        
+        if self.actual_time in self._avaiable_range:
+            print("Okay, it's time to Pomo!\r\n")
+            
+            if input("Would you like to start your pomo? (yes|no) ").lower().strip() == "yes":
+                print("\r\nLet's configurate your Pomo!")
+                print("If you want you can use our standard Pomo configurations (Just press Enter)")
+                print("But if you prefer to configurate your Pomo (Pomo)")
+                
+                if input("What do you prefer? ").lower().strip() == "":
+                    self.pomodoro()
                 else:
-                    print("Well, it's not time for Pomo.")
+                    number = 0
+                    duration = 0
+                    small_rest = 0
+                    self.pomodoro(number, duration, small_rest)
 
+        else:
+            print("Well, it's not time for Pomo.")
+            print(self.next_pomo)
+
+    def next_pomo(self):
+        time_h = self.actual_time
+        hours = 0
+        
+        for _ in range(24):
+            if time_h in self.available_range:
+                return "Your next pomo is in {hours} hours."
+            else:
+                hours += 1
+                time_h += 1
+
+
+
+        
            
         
             
